b7f7af48791af3d9fe5709907b38305d5ddaceb1
DmenT2602 <karlos2602@hotmail.com>
2024-03-17 11:01:30 +0100

    Diseño y funcionamiento button Encriptar

---
+++ app.js
@@ -0,0 +1,30 @@
+function encriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("img-muneco");
+    
+    //Cifrado de palabras 
+    //Cambia las vocales por las palabras con comillas. -mama, papa, hijo...-
+    let textoCifrado = texto
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
+
+    if (texto.length != 0) {    
+    document.getElementById("texto").value = textoCifrado;
+    tituloMensaje.textContent = "Texto Encriptado con Exito!!";
+    parrafo.textContent = "";
+    muñeco.src = "./images/encriptar.svg";
+    
+ } else {
+    
+    muñeco.src = "./images/muñeco.png";
+    tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+    parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+    swal("ups!", "Debes ingresar un texto", "warning"); //Mensaje de alerta 
+    
+ }
+}
